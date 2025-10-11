c03cf7982f1ab56e29c08ac2028c4c62652b172e
ahmeteminpehlivan <ahmeteminpehlivan@gmail.com>
2025-10-11 12:58:06 +0300

    student model güncelleme

--- models/Student.js
+++ models/Student.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
   fullname: { type: String, required: true }, 
-  birthdate: { type: Date, required: true },
-  parentname: { type: String, required: true },
-  parentphone: { type: String, required: true },
+  birthDate: { type: Date, required: true },
+  parentName: { type: String, required: true },
+  parentPhone: { type: String, required: true },
 }, { timestamps: true });
 
 export default mongoose.model("Student", studentSchema);
